836da227bb0aee02e6499544715516024122f1d5
aMasashiUmeda <masashi.umeda@access-company.com>
2017-12-11 13:51:33 +0900

    react-selectのコードを追加

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Select from 'react-select';
+import _ from 'lodash';
+import 'react-select/dist/react-select.css';
+
+import Styles from './App.css';
 
 class App extends Component {
+  state = {
+    selectedOption: null,
+  }
+
+  onChange = (selectedOption) => {
+    this.setState({ selectedOption });
+  }
+
   render() {
+    const options = [
+      { value: 'nfb', label: 'NetFront Browser' },
+      { value: 'nfnx', label: 'NetFront NX' },
+      { value: 'nfbe', label: 'NetFront BE' },
+    ];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={Styles.app}>
+        <Select
+          name="form-field-name"
+          value={_.get(this.state, 'selectedOption.value', '')}
+          onChange={this.onChange}
+          options={options}
+        />
       </div>
     );
   }

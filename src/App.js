f78f2fd857ad512f90125b7c640d3991b4fc52ad
aMasashiUmeda <masashi.umeda@access-company.com>
2017-12-11 18:46:13 +0900

    Async componentのサンプルを追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
 import _ from 'lodash';
+import axios from 'axios';
 import 'react-select/dist/react-select.css';
 
 import Styles from './App.css';
+
+function getUsers (input) {
+  if (!input) {
+    return Promise.resolve({ options: [] });
+  }
+
+  return axios.get(`https://api.github.com/search/users?q=${input}`)
+  .then((response) => {
+    return { options: _.get(response, 'data.items', []) };
+  });
+}
 
 class App extends Component {
   state = {
@@ -14,6 +26,10 @@ class App extends Component {
     this.setState({ selectedOption });
   }
 
+  onChangeUser = (value) => {
+    this.setState({ value });
+  }
+
   render() {
     const options = [
       { value: 'nfb', label: 'NetFront Browser' },
@@ -22,12 +38,27 @@ class App extends Component {
     ];
     return (
       <div className={Styles.app}>
-        <Select
-          name="form-field-name"
-          value={_.get(this.state, 'selectedOption.value', '')}
-          onChange={this.onChange}
-          options={options}
-        />
+        <div className={Styles.box}>
+          Standard
+          <Select
+            name="form-field-name"
+            className={Styles.list}
+            value={_.get(this.state, 'selectedOption.value', '')}
+            onChange={this.onChange}
+            options={options}
+          />
+        </div>
+        <hr />
+        <div className={Styles.box}>
+          Server Request
+          <Select.Async
+            value={this.state.value}
+            onChange={this.onChangeUser}
+            valueKey="id"
+            labelKey="login"
+            loadOptions={getUsers}
+          />
+        </div>
       </div>
     );
   }
